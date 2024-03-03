ce9e4835d9ececb37fe20b6ef09dd5d8ba19c3e9
Wojciech Bartnik <yisonPylkita@gmail.com>
2024-03-03 22:25:07 +0100

    Implemented very simple Runtime

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+use crate::runtime::Runtime;
+
 mod lexer;
+mod runtime;
 
 fn main() {
     let source_code = &r#"let x = 10; println("Hello there")"#.to_string();
     println!("Lexing code: {source_code}");
 
     let mut lex = lexer::Lexer::new();
-    let tokens = lex
+    let ast = lex
         .tokenize(source_code)
         .expect("Could not parse this code");
-    println!("{:?}", tokens);
+    println!("{:?}", ast);
+    println!("");
+
+    println!("Running this code now");
+    println!("----------------------");
+
+    let mut runtime = Runtime::new();
+    runtime.run(ast).expect("Cannot run this code");
+
+    println!("----------------------");
+    println!("");
+
+    println!("Variables in runtime memory after script execution");
+    for (k, v) in runtime.variables {
+        println!("{k}: {v:?}");
+    }
 }
